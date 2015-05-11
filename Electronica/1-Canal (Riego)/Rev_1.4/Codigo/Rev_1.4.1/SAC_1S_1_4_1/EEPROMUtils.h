29df42f7b9c64902121e7e7790bd0b8e7be6135c
surcadiz <surcadiz@users.noreply.github.com>
2015-05-11 12:59:26 +0200

    Update EEPROMUtils.h

--- Electronica/1-Canal (Riego)/Rev_1.4/Codigo/Rev_1.4.1/SAC_1S_1_4_1/EEPROMUtils.h
+++ Electronica/1-Canal (Riego)/Rev_1.4/Codigo/Rev_1.4.1/SAC_1S_1_4_1/EEPROMUtils.h
@@ -1,14 +1,15 @@
+#include <EEPROM.h>
 /****************************************************************
 EEPROMUtils: this file contains all the information about the Configuration
  for the SAC Project:
   http://sacultivo.com
-  
-  IN this file there is the functions for use the EEPROM for store the 
+
+  IN this file there is the functions for use the EEPROM for store the
   configuration.
-  
+
   Author: Victor Suarez Garcia<suarez.garcia.victor@gmail.com>
   Co-Author: David Cuevas Lopez<mr.cavern@gmail.com>
-  
+
   * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -28,62 +29,59 @@ EEPROMUtils: this file contains all the information about the Configuration
  in the EEPROM.
  * Current Version: 0.2.
 *************************************************************************/
- #include <EEPROM.h>
- #define CONFIG_START 25 //Initial Address in the EEPROM.
- 
-// Struct with the configuration
 
-typedef struct{
-  int Relay;//Relay
-  float SMOp; //Soil Moisture Optimum
-  float SMmin;//Soil Moisture minimum
-  float STMax;//Maximum Soil temperature
-  float STmin;//Minimum Soil Temperature
-  int PICicle;//Percentaje Irrigation Cicle.
-  float SM_calib;//SM calibration
-  int check_interval; //Interval Check for update the Sensors
-  int flow_diameter;//Flow Diameter for Flow Size Sensor
-  int active_languaje;//Active Languaje
-  int interval_time;//IntervalTime
-  int TICicle;
-  
-}Configuration;
+#define CONFIG_START 25 //Initial Address in the EEPROM.
 
-/*
- get the default configuration.
-  returns: Default Configuration.
- */
-Configuration getDefaultConfig(){
- Configuration default_config;
-  default_config.Relay=0;
-  default_config.SMOp=60;
-  default_config.SMmin=40;
-  default_config.STMax=36.0;
-  default_config.STmin=6.0;
-  default_config.TICicle=6;
-  default_config.PICicle=50;
-  default_config.check_interval=15;
-  default_config.flow_diameter=16;
-  default_config.active_languaje=0;
-  default_config.SM_calib=500;
+//Struct with the configuration
 
-  return default_config;
+typedef struct
+{
+	int Relay;//Relay
+	float SMOp; //Soil Moisture Optimum
+	float SMmin;//Soil Moisture minimum
+	float STMax;//Maximum Soil temperature
+	float STmin;//Minimum Soil Temperature
+	int PICicle;//Percentaje Irrigation Cicle.
+	float SM_calib;//SM calibration
+	int active_languaje;//Active Languaje
+	int TICicle;
+
+} Configuration;
+
+// get the default configuration. Returns: Default Configuration.
+
+Configuration getDefaultConfig()
+{
+	Configuration default_config;
+	default_config.Relay = 0;
+	default_config.SMOp = 60;
+	default_config.SMmin = 40;
+	default_config.SM_calib = 500;
+	default_config.STMax = 36.0;
+	default_config.STmin = 6.0;
+	default_config.TICicle = 6;
+	default_config.PICicle = 50;
+	default_config.active_languaje = 0;
+
+
+	return default_config;
 }
 /*
  * store the Settings in the EEPROM
  * Parameters:
  * Settings: The Settings to Store.
- * Returns: The number of bytes writed*/  
+ * Returns: The number of bytes writed*/
 int store_Settings(Configuration & settings)
 {
-   int e=CONFIG_START;
-   int i=0;
-   byte* data=(byte *)&settings;
-   for(i=1;i<sizeof(settings);i++){
-     byte b=data[i];
-     EEPROM.write(e+i,b);
-   }
-   return i;
+	int e = CONFIG_START;
+	int i = 0;
+	byte* data = (byte *)&settings;
+	for(i = 1; i < sizeof(settings); i++)
+	{
+		byte b = data[i];
+		EEPROM.write(e + i, b);
+	}
+	return i;
 }
 /*
 * loads the Settins form The EEPROM.
@@ -92,21 +90,23 @@ int store_Settings(Configuration & settings)
 * returns: number of bytes readed.*/
 int load_Settings(Configuration & settings)
 {
-  int e=CONFIG_START;
-   int i=0;
-   byte* data=(byte *)&settings;
-   for(i=1;i<sizeof(settings);i++){
-     data[i]=EEPROM.read(e+i);
-   }
-   memcpy(&settings,data,sizeof(settings));
-   return i;
+	int e = CONFIG_START;
+	int i = 0;
+	byte* data = (byte *)&settings;
+	for(i = 1; i < sizeof(settings); i++)
+	{
+		data[i] = EEPROM.read(e + i);
+	}
+	memcpy(&settings, data, sizeof(settings));
+	return i;
 }
 /**
 * loads the default configuration, and store it in the EERPROM
 * Returns: default configuration settings.
 */
-Configuration reset_Settings(){
-  Configuration current_config=getDefaultConfig();
-  store_Settings(current_config);
-  return current_config;
+Configuration reset_Settings()
+{
+	Configuration current_config = getDefaultConfig();
+	store_Settings(current_config);
+	return current_config;
 }
